80eb911e2251db90c6fd58e04811ca428955537d
farzin89 <farzin_ahmadi90@yahoo.com>
2022-07-05 16:31:23 +0200

    check the price every 4 sec and let me know

--- API.py
+++ API.py
@@ -1,6 +1,22 @@
 
 import requests
+import time
+
+def inform_farzin():
+    print("hi there,price is good")
+
+def inform_farzin2():
+    print('price is not good')
+
+my_good_price = 19559
 
 response = requests.get("https://api.coinbase.com/v2/prices/buy?currency=USD")
+price = float(response.json()['data']['amount'])
+print('at this moment, bitcoin is %i dollars' %price)
+while True :
+    if (price < my_good_price):
+        inform_farzin()
+    else :
+        inform_farzin2()
+    time.sleep(4)
 
-print('in this moment, bitcoin is %i dollars' %float((response.json()['data']['amount'])))
